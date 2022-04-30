93e9ece2b67dbd63355d4c837244dab6a23d6958
MASUD-01 <masudrana96141@gmail.com>
2022-04-30 21:34:19 +0600

    create navbar

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            this is homedfsfbbbb
+        </div>
+    );
+};
+
+export default Home;
